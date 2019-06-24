2725f85cb743170f925d62a761f758ff47c1a1c4
刘为 <wei.liu@g-town.com.cn>
2019-06-24 17:02:09 +0800

    更新插件版本

--- redis/redis.js
+++ redis/redis.js
@@ -1,22 +1,24 @@
 import redis from "redis";
 import config from "config-lite";
-import log from "../web/util/log4jsUtil";
+import logger from "../web/util/log4jsUtil";
 
 
 
 const client = redis.createClient();
 
+
 client.on("ready",function(err){
     if( err ){
-        console.error(` redis error  ${ err }`);
+        console.error(`redis error  ${ err }`);
     }else{
-        log.info("redis连接成功， 端口号:6379");
+        logger.info("redis连接成功， 端口号:6379");
     }
 })
 
 
 client.on("error", function (err) {
-    console.error(` redis error  ${ err }`);
+    console.log("error =============  " ,  log );
+    logger.error(`redis error  ${ err }`);
 });
 
 
